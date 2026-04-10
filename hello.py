0073068abc4e680cb5af875a2afe2bea8f32f9e5
Balaram-R <ramjibalaram@gmail.com>
2026-04-10 11:54:55 +0530

    Added Latest files

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 # hello.py
+# My first file in Agentic AI Journey
+
 def greet(name):
+    """Greet the user."""
     return f"Hello {name}! Welcome to your Agentic AI Journey. 🚀"
 
+
 if __name__ == "__main__":
-    print(greet("Balaram"))
-    print("Week 1 completed - Environment + First Commit")
+    print(greet("Balaram]]]]"))
+    print("Week 1 completed - Environment + First Commit") 
